51fc50aac54d7abdcf77d5c8f1130b5f26cd1964
Kanishka Baasu MB <mbkanishkabaasu08@gmail.com>
2025-07-28 21:06:34 +0530

    Positive Negative completed

---
+++ Positivenegative.c
@@ -0,0 +1,17 @@
+#include  <stdio.h>
+
+void main()
+{
+    int num;
+    printf("Enter num:");
+    scanf("%d",&num);
+
+    if(num >= 0)
+    {
+    printf("Positive");
+    }
+    else
+    {
+    printf("Negative");
+    }
+}
