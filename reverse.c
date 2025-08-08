8f18ce82ddef5da926d47ffaf260a35faa83b980
Kanishka Baasu MB <mbkanishkabaasu08@gmail.com>
2025-08-08 18:55:58 +0530

    reverse completed

---
+++ reverse.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void main()
+{
+    int n , copy,rev=0;
+
+    printf("Enter n: ");
+    scanf("%d",&n);
+
+    copy=n;
+
+    while(copy>0)
+    {
+        rev = rev*10;
+
+        rev = rev+(copy%10);
+        copy/=10;
+    }
+
+    if(n==rev)
+    {
+        printf("Palindrome");
+
+    }
+    else
+    {
+        printf("Not a Palindrome");
+    }
+}
