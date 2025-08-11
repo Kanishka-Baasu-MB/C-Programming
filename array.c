c2cc65502ffbe2ef7eb414093bf4418035ca3de9
Kanishka Baasu MB <mbkanishkabaasu08@gmail.com>
2025-08-11 20:44:27 +0530

    array completed

---
+++ array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+    int heights[50];
+
+    heights[0]=23778259;
+    heights[1]=768484852;
+    heights[2]=4585857474;
+
+    printf("%d\n",heights[0]);
+    printf("%d\n",heights[1]);
+    printf("%d\n",heights[3]);
+
+    heights[0]=7485;
+    printf("%d\n",heights[0]);
+}
