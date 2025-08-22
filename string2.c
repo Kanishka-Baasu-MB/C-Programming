0e7976fa073f0f1c354286f626b0e9b1f14021c1
Kanishka Baasu MB <mbkanishkabaasu08@gmail.com>
2025-08-22 22:01:56 +0530

    string 2 completed

---
+++ string2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{
+    int n;
+    char str[50],str2[5];
+
+    printf("Enter String:");
+    scanf("%s",&str);
+
+    printf("Enter String2:");
+    scanf("%s",&str2);
+
+
+
+    printf("%s \n",str);
+
+
+}
