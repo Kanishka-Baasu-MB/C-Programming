d41a3dc6b35bd1e21c70b226bc87ba226c45120f
Kanishka Baasu MB <mbkanishkabaasu08@gmail.com>
2025-08-24 21:12:53 +0530

    2d array completed

---
+++ 2d array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main()
+{
+    int arr[2][2],i,j;
+
+    for(i=0;i<2;i++)
+    {
+        for (j=0;j<2;j++)
+        {
+            printf("Enter Element:");
+            scanf("%d",&arr[i][j]);
+        }
+    }
+for(i=0;i<2;i++)
+    {
+        for (j=0;j<2;j++)
+        {
+            printf("Enter Element:");
+        }
+        printf("\n");
+    }
+}
